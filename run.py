f07254896a1750bd69332c85fb7fd95355533d07
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-10-11 02:20:49 +0200

    Feat(google-sheets): Add Google Sheets API integration and setup

--- run.py
+++ run.py
@@ -1,5 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
 from colorama import Fore, Back, Style
 from inventory import Inventory
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('inventory')
+
+sales = SHEET.worksheet('sheet')
+
+data = sales.get_all_values()
+
+print(data)
 
 
 inventory = Inventory()
