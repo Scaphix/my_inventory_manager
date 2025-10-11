842a2f3f40997fc5340994b376df0f203444034b
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-10-11 11:50:19 +0200

    Add main() function for cleaner program execution

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from google.oauth2.service_account import Credentials
 from colorama import Fore, Back, Style
 from inventory import Inventory
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -14,35 +15,45 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('inventory')
 
-sales = SHEET.worksheet('sheet')
+# sales = SHEET.worksheet('sheet')
+# data = sales.get_all_values()
 
-data = sales.get_all_values()
-
-print(data)
+# print(data)
 
 
 inventory = Inventory()
+
+
+def main():
+    """
+    Welcome Message
+    """
+    # Clear the console when the program starts
+    print("\033c", end="")
+    print(Back.BLUE + "\n Welcome to the Inventory Managment tool\n"
+          + Style.RESET_ALL
+          )
+    print(
+            "This tool helps you easily manage your stock-"
+            "add, view, update, or delete items using their ID.\n"
+            "Follow the prompts on screen to perform any action "
+            "and keep your inventory up to date.\n"
+            )
+    print("-" * 40)
 
 
 def tasks_list():
     """Display the menu and handle user input"""
     program_running = True
     while program_running:
-        print(Back.BLUE + "\n Welcome to the Inventory Managment tool\n")
-        print(Style.RESET_ALL)
-        print(
-            "This tool helps you easily manage your stock-"
-            "add, view, update, or delete items using their ID.\n"
-            "Follow the prompts on screen to perform any action "
-            "and keep your inventory up to date.\n"
-            )
-        print("-" * 40)
-        print("Please select one of the following tasks:\n")
-        print(Fore.BLUE + Style.BRIGHT + "[1] Add item")
-        print("[2] Update item")
-        print("[3] Display item")
-        print("[4] Delete item")
-        print("[5] EXIT\n" + Style.RESET_ALL)
+        print("\nPlease select one of the following tasks:")
+        print("-" * 38)
+        print(Fore.BLUE + Style.BRIGHT + """
+        [1] Add item
+        [2] Update item
+        [3] Display item
+        [4] Delete item
+        [5] EXIT\n""" + Style.RESET_ALL)
         selected_task = input("Select task : ").strip()
         print(f"your selection is: {selected_task}")
         go_on = my_task(selected_task)
@@ -51,11 +62,14 @@ def tasks_list():
 
 
 def my_task(selected_task):
-
+    """
+    Task or choice manager,
+    each number chosen will excecute the corresponding method
+    """
     if selected_task == "1":
         print("\033c", end="")
-        print(Fore.BLUE + Style.BRIGHT + "\n--- Add a New Item ---")
-        print(Style.RESET_ALL)
+        print(Fore.BLUE + Style.BRIGHT + "\n--- Add a New Item ---"
+              + Style.RESET_ALL)
         while True:
             inventory.add_item()
             more = input("Add another item? (y/n:) ").strip().lower()
@@ -63,6 +77,8 @@ def my_task(selected_task):
                 break
 
     elif selected_task == "2":
+        print(Fore.BLUE + Style.BRIGHT + "\n--- Update an existing Item ---"
+              + Style.RESET_ALL)
         inventory.update_item()
 
     elif selected_task == "3":
@@ -70,9 +86,11 @@ def my_task(selected_task):
         inventory.display_item()
 
     elif selected_task == "4":
-        print("Please enter the id of the item to delete ")
+        print(Fore.BLUE + Style.BRIGHT + "\n--- Delete an existing Item ---"
+              + Style.RESET_ALL)
+        print("Please enter the ID of the item to delete ")
         try:
-            id_to_delete = int(input("Enter Id: ").strip())
+            id_to_delete = int(input("Enter ID: ").strip())
             inventory.delete_item(id_to_delete)
         except ValueError:
             print(Fore.RED + "Invalid ID! Please enter a number.")
@@ -82,8 +100,16 @@ def my_task(selected_task):
         print(" Exiting the program .... Goodbye !")
         return False
     else:
-        print("Invalid selection. Please choose 1-5.")
+        print(Fore.RED + "Invalid selection. Please choose 1-5."
+              + Style.RESET_ALL)
     return True
 
 
-task = tasks_list()
+"""
+This loop Runs only when this file is executed directly
+main() runs once at program start
+task_list() starts the interactive menu
+"""
+if __name__ == "__main__":
+    main()
+    tasks_list()
